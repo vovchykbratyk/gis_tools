4327304962c1bc101fc3927b3a3b1bcc1c8ef151
vovchyk bratyk <40668075+vovchykbratyk@users.noreply.github.com>
2022-11-23 14:57:44 -0500

    add network failure exception

--- scripts/utils/exceptions.py
+++ scripts/utils/exceptions.py
@@ -2,7 +2,15 @@
 Custom exceptions
 """
 
+
+class ExceptionNetworkFailure(Exception):
+    """
+    Raised upon network timeout or other failure
+    """
+    pass
+
 class PkiPasswordError(Exception):
     """
     Raised when no password is provided to PKI challenge
     """
+    pass
