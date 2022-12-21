6a39c73e63243943646dde9cdc920b0b4f12eb2e
VovchykBratyk <eric@eaglefamily.io>
2022-12-21 12:59:14 -0500

    fix docstring

--- scripts/blender/dae2dae.py
+++ scripts/blender/dae2dae.py
@@ -4,10 +4,9 @@ to convert a Collada 1.5 object (output from ArcGIS Pro but not widely
 used in practice) to a Collada 1.4 object (which is more widely used).
 The script is issued as an argument to an invocation of a background
 (no GUI) instance of Blender, e.g.:
-"""
 
 /path/to/blender.exe -b -P dae2dae.py file_to_be_converted.dae
-
+"""
 
 import bpy
 import sys
