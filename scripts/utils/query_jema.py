ccc659958f29b9f62d49b6c593b7506274daf6a9
VovchykBratyk <eric@eaglefamily.io>
2023-07-19 16:02:39 -0400

    update query_jema.py

--- scripts/utils/query_jema.py
+++ scripts/utils/query_jema.py
@@ -1,11 +1,10 @@
-import arcpy
 import json
 import os
 from pathlib import Path
 from uuid import uuid4
 
-from arcpki import ArcPKI
 
+# Globals
 CFG_DIR = os.path.join(Path(os.path.dirname(__file__)).parent, "cfg")
 JEMA_CONFIG_FILE = os.path.join(CFG_DIR, "jema_config.json")
 
@@ -20,12 +19,12 @@ class JEMA:
         self.root = self.cfg["root"]
         self.model_id = self.cfg["model_id"]
         self.paramset = self.cfg["input_parameter_set"]
-        self.temp = self.cfg["usr_temp_path"]
+        self.temp = os.path.join(
+            os.path.expanduser('~'),
+            '.igea',
+            self.cfg["usr_temp_path"])
         if not os.path.exists(self.temp):
-            try:
-                os.makedirs(self.temp)
-            except PermissionError:
-                self.temp = "U:/"  # Profile fallback
+            os.makedirs(self.temp)
               
     def jema_cfg(self):
         with open(JEMA_CONFIG_FILE, "r") as cf:
@@ -36,8 +35,8 @@ class JEMA:
         Parameters:
         - startdate (YYYY-mm-ddTHH:MM:SSZ)
         - stopdate (same as above)
-        - EITHER pointwkt OR polywkt (e.g., POINT(10 20)  )
-        - pointwkt must have additional param radiuskm expressed in km (e.g., 25)
+        - simple Point OR Polygon WKT (e.g., POINT(10 20)  )
+        - Point WKT must have radiuskm expressed in km (e.g., 25)
         """
         payload = []
         for k, v in kwargs.items():  # Set up parameters
@@ -45,7 +44,7 @@ class JEMA:
                 if k == pn:  # parameter names match, get the jema parameter value and build the query string
                     payload.append(f"{pv}={str(v).replace(':', '%3A').replace(' ', '%20')}")
         jema_url = f"{self.root}{self.model_id}&{self.paramset}&{'&'.join(payload)}"
-        s = kwargs["session"]  # pass in the geoaxis session token for reuse
+        s = kwargs["session"]  # catch the geoaxis session token for reuse
         model_r = s.get(jema_url)
         fn = os.path.join(self.temp, f"{uuid4()}.geojson")
         with open(fn, 'wb') as gj:
